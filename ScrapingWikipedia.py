21888413f9fea34bf3697f54e1730103594589a8
Edouard Vincent <75450487+EdouardVincent@users.noreply.github.com>
2022-03-30 22:42:43 +0200

    Add files via upload

---
+++ ScrapingWikipedia.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+conn = sqlite3.connect('database.sqlite')
+cur = conn.cursor()
+
+cur.execute ('''DROP TABLE IF EXISTS classement''')
+cur.execute('''CREATE TABLE classement (id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, pays TEXT, link TEXT)''')
+
+url = 'https://fr.wikipedia.org/wiki/Liste_des_pays_par_population'
+
+reponse = requests.get(url)
+
+if reponse.ok: #Response[200]
+    
+    links = []
+    soup = BeautifulSoup(reponse.text)
+    title = soup.find('title')
+
+    x=0
+    tds = soup.findAll('td')
+    for td in tds :
+
+        a = td.find('a')
+        if a != None :
+            x+=1
+            if x < 237 :
+                a = td.find('a')
+                link = a['href']
+            
+            c = link.split('/wiki/Fichier:Flag_of_')
+            
+            if c[0] == '' :
+                link = c[1]
+
+                d = link.split('.svg')
+
+                link = d[0]
+ 
+            
+            whole_link = ('https://fr.wikipedia.org/wiki/' + link)
+
+            cur.execute('''INSERT INTO classement(pays, link) VALUES (?,?)''',(link, whole_link))
+
+            
+        
+    for row in cur.execute('''SELECT id, pays, link FROM classement'''):
+        print(row)
+
+cur.close()
+        
+
+
+        
